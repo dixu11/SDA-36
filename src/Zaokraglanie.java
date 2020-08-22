685a6ad181a9b47920d3cb05c63cfe42023a246e
Daniel Szlicht <daniel.szlicht25@gmail.com>
2020-08-22 11:21:51 +0200

    zaokrąglanie

---
+++ src/Zaokraglanie.java
@@ -0,0 +1,18 @@
+public class Zaokraglanie {
+    public static void main(String[] args) {
+        double liczba = 3.894;
+        int liczbaInt = (int)liczba;
+        System.out.println(liczbaInt);
+
+      int zaokraglona =  (int) Math.round(liczba);
+        System.out.println(zaokraglona);
+
+       /* liczba *= 100;
+        int liczbaJakoInt = (int) liczba;
+        liczba /= 100;
+        System.out.println(liczbaInt);*/
+
+        System.out.printf("Moja liczba to: %.1f", liczba);
+
+    }
+}
