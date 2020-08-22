bec129bbc760c0cf01609dd9c4fd9bb2ad0cd62a
Daniel Szlicht <daniel.szlicht25@gmail.com>
2020-08-22 11:37:13 +0200

    pętle

---
+++ src/Petle.java
@@ -0,0 +1,26 @@
+public class Petle {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Daniel");
+        }
+
+
+        int index = 0;
+        while (index < 5){
+            System.out.println("Daniel");
+            index++;
+        }
+
+
+
+
+
+
+
+        for (int i = 0; i < 10; i++) {
+
+        }
+
+
+    }
+}
