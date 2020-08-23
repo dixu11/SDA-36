06f07d0f3c42d5f8be6ee617398fe0af04134539
Daniel Szlicht <daniel.szlicht25@gmail.com>
2020-08-23 11:35:32 +0200

    metody podstawa

--- src/metody/Metody.java
+++ src/metody/Metody.java
@@ -1,7 +1,31 @@
 package metody;
 
 public class Metody {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        przygotowanieSkladnikow();
+        gotowanie();
+        podanie();
     }
+
+    static void przygotowanieSkladnikow() {
+        System.out.println("Potrzebne składniki:");
+        System.out.println("Jajka");
+        System.out.println("Chleb");
+        System.out.println("Masło");
+    }
+
+    static void gotowanie() {
+        System.out.println("Przygotowanie:");
+        System.out.println("Rozbić jajka");
+        System.out.println("smażenie");
+        System.out.println("Smarowanie chleba...");
+    }
+
+    static void podanie() {
+        System.out.println("Podanie");
+        System.out.println("Wiadomo...");
+    }
+
+
 }
