27ca5d39787d75863e610b33ac99e7b5596fa7ca
Daniel Szlicht <daniel.szlicht25@gmail.com>
2020-09-12 11:56:24 +0200

    dziedziczenie polimorfizm, downcasting, upcasting i instanceof

--- src/obiektowosc/dziedziczenie/Demo.java
+++ src/obiektowosc/dziedziczenie/Demo.java
@@ -6,28 +6,52 @@ import java.util.List;
 public class Demo {
     public static void main(String[] args) {
         //Kot
-        Kot kot = new Kot("Lucyna",10);
-        //Pies
-        Pies pies = new Pies();
+        Zwierze animal = new Pies("Lucyna", 10);
+        Zwierze animal2 = new Kot();
         Kon kon = new Kon("Radek", 7);
-        System.out.println(kot);
-        System.out.println(pies);
+        System.out.println(animal);
+        System.out.println(animal2);
         System.out.println(kon);
 
-        kot.polujNaMyszy();
-        kot.biegaj();
-        pies.aportuj();
+
+        //animal.polujNaMyszy();
+        //animal.biegaj();
+        animal.wydajDzwiek();
+        animal2.wydajDzwiek();
+
+        if (animal instanceof Pies) {
+            System.out.println("Animal to pies");
+        } else {
+            System.out.println("Animal to inne zwierze");
+        }
+
+
+        //gorszy kod bez zastosowania polimorfizmu
+     /*   if (animal == pies) {
+            animal.szczekaj();
+        } else if (animal == kot) {
+            animal.mialcz();
+        }*/
+
+
+        // animal2.aportuj();
 
         //Zwierze
         System.out.println("Wszystkie zwierzeta:");
         List<Zwierze> zwierzeta = new ArrayList<>();
-        zwierzeta.add(kot);
-        zwierzeta.add(pies);
+        zwierzeta.add(animal);
+        zwierzeta.add(animal2);
         zwierzeta.add(kon);
         for (Zwierze zwierze : zwierzeta) {
             zwierze.biegaj();
             zwierze.wydajDzwiek();
         }
+
+        Pies animalJakoPies = (Pies) animal; //downcasting
+       // animal.aportuj(); // nie działa bo po Zwierze
+       animalJakoPies.aportuj(); // działa bo po Pies
+        Zwierze animal3 = kon; // upcasting
+
 
     }
 }
